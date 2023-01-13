7f69a6259af8de02e3a5b509a887590370c3d81a
SumeyraAltinisik <sfa2023@gmail.com>
2023-01-13 19:26:36 +0300

    Add project files.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp13
+{
+    internal class Program
+    {
+        //TODO: Daha çook iş var. 
+        static void Main(string[] args)
+        {                            
+            Program p=new Program();
+            p.func1();
+        }
+        public void func1()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i);
+                System.Threading.Thread.Sleep(2000);
+                if(i == 8)
+                {
+                    System.Diagnostics.Debugger.Launch();
+                }
+
+            }
+        }
+    }
+}
